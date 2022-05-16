6d2ff006563c39a5601ace9ff72d19531ff6d695
José792 <joseph_camilo@ucne.edu.do>
2022-05-15 20:11:00 -0400

    Agregando proyecto

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+ int n, entero;
+            double dec;
+            Console.Write("Ingrese un #: ");
+            n = Convert.ToInt32(Console.ReadLine());
+
+            entero = n / 2;
+            dec = (double)n / 2;
+            if (entero - dec == 0)
+            {
+                Console.WriteLine("El numero {0} es par.", n);
+            }
+
+            else
+            {
+                Console.WriteLine("El numero {0} es impar.", n);
+
+            }// fin del if
+            Console.ReadKey();
